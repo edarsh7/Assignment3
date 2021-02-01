f61e6998ded1d262ff607cf4314224c83f6a83c3
edarsh7 <edarsh7@gmail.com>
2021-02-01 15:00:26 -0800

    idk

--- manpage.c
+++ manpage.c
@@ -14,8 +14,8 @@
 
 int turn = 0;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INTIALIZER;
 
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 void *thread_organizer(void *empty)
 {
   int pid = getParagraphId();
