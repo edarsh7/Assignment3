b52a4b7e16408dd49ba31a7d3c9a639d422e2e38
edarsh7 <edarsh7@gmail.com>
2021-02-01 14:57:59 -0800

    idk

--- manpage.c
+++ manpage.c
@@ -14,7 +14,7 @@
 
 int turn = 0;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INTIALIZER
+pthread_cond_t cond = PTHREAD_COND_INTIALIZER;
 
 void *thread_organizer(void *empty)
 {
