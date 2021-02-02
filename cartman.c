a7db1e1e124d3f282123d4a15c0577c6d63fab83
edarsh7 <edarsh7@gmail.com>
2021-02-02 15:57:50 -0800

    idk

--- cartman.c
+++ cartman.c
@@ -1,11 +1,20 @@
 
 #include "cartman.h"
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+
+sem_t junction[5];
 
 /*
  * You need to implement this function, see cartman.h for details 
  */
 void arrive(unsigned int cart, enum track track, enum junction junction) 
 {
+  pthread_t thread;
+  int junct = (int) junction;
+  pthread_create(&thread, NULL, arrive_manager, (void)* junct);
+  pthread_join(&thread, NULL);
 }
 
 /*
@@ -15,12 +24,21 @@ void depart(unsigned int cart, enum track track, enum junction junction)
 {
 }
 
+
 /*
  * You need to implement this function, see cartman.h for details 
  */
 void cartman() 
 {
-  // Remove, only here to fail code warning test
-  int foo = 1024;
+  for(int i=0; i < 5; i++)
+  {
+    sem_init(&junction[i], 0, 1);
+  }
 }
 
+void *arrive_manager(void *arg)
+{
+  int junct = *(int *)arg;
+  sem_wait(&junction[junct]);
+  sem_wait(&junction[junct+1]);
+}
