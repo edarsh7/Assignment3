cd4e5d3838137e1b3e3b6ce42373e3f4819d1e5c
edarsh7 <edarsh7@gmail.com>
2021-02-02 19:22:45 -0800

    idk

--- cartman.c
+++ cartman.c
@@ -17,6 +17,43 @@ void *arrive_manager(void *arg)
 {
   cart_info *CART = (cart_info *)arg;
 
+  switch(CART->track){
+    case Black:
+      sem_wait(&junction[0]);
+      sem_wait(&junction[4]);
+      reserve(CART->cart, A);
+      reserve(CART->cart, E);
+      break;
+
+    case Red:
+      sem_wait(&junction[0]);
+      sem_wait(&junction[1]);
+      reserve(CART->cart, A);
+      reserve(CART->cart, B);
+      break;
+
+    case Green:
+      sem_wait(&junction[1]);
+      sem_wait(&junction[2]);
+      reserve(CART->cart, B);
+      reserve(CART->cart, C);
+      break;
+
+    case Blue:
+      sem_wait(&junction[2]);
+      sem_wait(&junction[3]);
+      reserve(CART->cart, C);
+      reserve(CART->cart, D);
+      break;
+
+    case Yellow:
+      sem_wait(&junction[3]);
+      sem_wait(&junction[4]);
+      reserve(CART->cart, D);
+      reserve(CART->cart, E);
+      break;
+  }
+  cross(CART->cart, CART->track, CART->junction);
 
 }
 
@@ -33,6 +70,7 @@ void arrive(unsigned int cart, enum track track, enum junction junction)
   pthread_t thread;
   
   pthread_create(&thread, NULL, arrive_manager, (void *) &CART);
+  pthread_join(&thread, NULL);
 }
 
 /*
@@ -40,7 +78,42 @@ void arrive(unsigned int cart, enum track track, enum junction junction)
  */
 void depart(unsigned int cart, enum track track, enum junction junction) 
 {
+    switch(CART->track){
+    case Black:
+      release(cart, A);
+      release(cart, E);
+      sem_open(&junction[0]);
+      sem_open(&junction[4]);
+      break;
 
+    case Red:
+      release(cart, A);
+      release(cart, B);
+      sem_open(&junction[0]);
+      sem_open(&junction[1]);
+      break;
+
+    case Green:
+      release(cart, B);
+      release(cart, C);
+      sem_open(&junction[1]);
+      sem_open(&junction[2]);
+      break;
+
+    case Blue:
+      release(cart, C);
+      release(cart, D);
+      sem_open(&junction[2]);
+      sem_open(&junction[3]);
+      break;
+
+    case Yellow:
+      release(cart, D);
+      release(cart, E);
+      sem_open(&junction[3]);
+      sem_open(&junction[4]);
+      break;
+  }
 }
 
 
