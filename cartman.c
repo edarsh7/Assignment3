9387de970e0fbdbf0ed40eb5608057ed58c06127
edarsh7 <edarsh7@gmail.com>
2021-02-03 00:47:44 -0800

    idk

--- cartman.c
+++ cartman.c
@@ -23,14 +23,15 @@ void *arrive_manager(void *arg)
   if(CART->track == Black)
   {
     sem_wait(&junction[4]);
-    sem_wait(&junction[0]);
     reserve(CART->cart, E);
+    
+    sem_wait(&junction[0]);
     reserve(CART->cart, A);
   }else
   {
     sem_wait(&junction[(int)CART->track]);
-    sem_wait(&junction[(int)CART->track + 1]);
     reserve(CART->cart, CART->track);
+    sem_wait(&junction[(int)CART->track + 1]);
     reserve(CART->cart, CART->track + 1);
   }
   
