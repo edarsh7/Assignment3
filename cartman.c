9f81560f471a9b679a0a5cde2431571f1873ab81
edarsh7 <edarsh7@gmail.com>
2021-02-03 16:12:43 -0800

    idk

--- cartman.c
+++ cartman.c
@@ -2,11 +2,10 @@
 #include "cartman.h"
 #include <pthread.h>
 #include <semaphore.h>
-#include <stdlib.h>
 #include <stdio.h>
 
 sem_t junction[5];
-pthread_t thread[5];
+sem_t deadlock;
 
 typedef struct cart_info
 {
@@ -15,53 +14,69 @@ typedef struct cart_info
   unsigned int cart;
 }cart_info;
 
-
 void *arrive_manager(void *arg)
 {
+
   cart_info *CART = (cart_info *)arg;
- 
-  sem_post(&junction[(CART->track) % 5]);
-  reserve(CART->cart, (CART->track) % 5);
-  sem_post(&junction[(CART->track + 1) % 5]);
-  reserve(CART->cart, (CART->track + 1) % 5);
+
+  
+  if(CART->track == Black)
+  {
+    sem_wait(&junction[4]);
+    reserve(CART->cart, E);
+    sem_wait(&junction[0]);
+    reserve(CART->cart, A);
+  }else
+  {
+    sem_wait(&junction[(int)CART->track]);
+    reserve(CART->cart, CART->track);
+    sem_wait(&junction[(int)CART->track + 1]);
+    reserve(CART->cart, CART->track + 1);
+  }
   
   cross(CART->cart, CART->track, CART->junction);
   return NULL;
 }
-
-
-
-
 
 /*
  * You need to implement this function, see cartman.h for details 
  */
 void arrive(unsigned int cart, enum track track, enum junction junction) 
 {
-  cart_info *CART = malloc(sizeof(cart_info));
-  CART->cart = cart;
-  CART->track = track;
-  CART->junction = junction;
+  cart_info CART;
+  CART.cart = cart;
+  CART.track = track;
+  CART.junction = junction;
 
+  pthread_t thread;
+  
 
-  pthread_create(&thread[cart], NULL, arrive_manager, CART);
+  pthread_create(&thread, NULL, arrive_manager, (void *) &CART);
+  pthread_join(thread, NULL);
 
 }
-
 
 /*
  * You need to implement this function, see cartman.h for details 
  */
 void depart(unsigned int cart, enum track track, enum junction junct) 
 {
-  release(cart, (track + 1) % 5);
-  sem_post(&junction[(track + 1) % 5]);
-  release(cart, (track) % 5);
-  sem_post(&junction[(track) % 5]);
+  
+  if(track == Black)
+  {
+    release(cart, A);
+    release(cart, E);
+    sem_post(&junction[0]);
+    sem_post(&junction[4]);
+  }
+  else
+  {
+    release(cart, track);
+    release(cart, track+1);
+    sem_post(&junction[(int)track]);
+    sem_post(&junction[(int)(track + 1)]);
+  }
 }
-
-
-
 
 
 /*
