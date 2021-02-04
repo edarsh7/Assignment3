34670cb8e5fd95840ea05102d6bde26b238229e1
edarsh7 <edarsh7@gmail.com>
2021-02-03 16:24:08 -0800

    idk

--- cartman.c
+++ cartman.c
@@ -52,8 +52,8 @@ void arrive(unsigned int cart, enum track track, enum junction junction)
   CART.track = track;
   CART.junction = junction;
 
-  pthread_create(&thread[cart], NULL, arrive_manager, (void *) &CART);
-  pthread_join(thread[cart], NULL);
+  pthread_create(&thread, NULL, arrive_manager, (void *) &CART);
+  pthread_join(thread, NULL);
 }
 
 /*
